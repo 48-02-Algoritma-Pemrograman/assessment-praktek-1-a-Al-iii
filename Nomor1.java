11d7d1aeab161399638e977c24284ca3f8c97367
Al-iii <anh191204@gmail.com>
2024-10-24 11:31:13 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,23 @@
-public class Nomor1{
-    public static void main(String[] args){
+import java.util.Scanner;
 
+public class praktek {
+    public static void main(String[] args) {
+        int jamLembur = 0;
+        double honorLembur = 0;
+        Scanner scanner= new Scanner(System.in);
+
+        jamLembur= scanner.nextInt();
+
+        for (int i = 1; i<=jamLembur; i++){
+        if (i<=16) {
+            honorLembur+=10000;
+        } else if (i>16) {
+            honorLembur+=15000;
+        }
+    }
+    System.out.println("Total jam lembur: "+ jamLembur);
+    System.out.println("Honor lembur: " + honorLembur);
+    
+    scanner.close();
     }
 }
