e8675d2df12734aa0c86b419f366e01bd90e1cec
Al-iii <anh191204@gmail.com>
2024-10-24 11:54:15 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -1,5 +1,47 @@
-public class Nomor2{
-    public static void main(String[] args){
+import java.util.Scanner;
+
+public class N2 {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        String namaDriver="", inputLagi="";
+        int lamaTrip=0, totalTrip=0;
+        double omzet=0;
+    
+        while (inputLagi=="Y") {
+            namaDriver=scanner.nextLine().toLowerCase();
+            lamaTrip=scanner.nextInt();
+            inputLagi=scanner.nextLine().toUpperCase();
+            
+        switch (namaDriver) {
+            case "beliau":
+                totalTrip+=lamaTrip;
+                break;
+            
+            case "mereka":
+                totalTrip+=lamaTrip;
+                break;
+
+            case "kita":
+                totalTrip+=lamaTrip;
+                break;
+        
+            default:
+                break;
+        }
+
+        switch (inputLagi) {
+            case "Y":
+                
+                break;
+
+            case "N":
+                
+                break;
+        
+            default:
+                break;
+        }
+    }
 
     }
 }
